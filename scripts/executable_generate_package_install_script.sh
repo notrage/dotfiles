08c2a11f7f13bc173726b789e37582ee935e61eb
NotRage <grousson.dylan@gmail.com>
2024-12-13 14:54:06 +0100

    RAAAAAAAAAAAH2

--- scripts/executable_generate_package_install_script.sh
+++ scripts/executable_generate_package_install_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Paths for the output scripts
-BREW_OUTPUT_SCRIPT="$HOME/.local/share/chezmoi/install_brew_packages.sh"
-APT_OUTPUT_SCRIPT="$HOME/.local/share/chezmoi/install_apt_packages.sh"
+BREW_OUTPUT_SCRIPT="$HOME/.local/share/chezmoi/run_install_brew_packages.sh"
+APT_OUTPUT_SCRIPT="$HOME/.local/share/chezmoi/run_install_apt_packages.sh"
 
 # Generate Homebrew installation script for macOS
 if command -v brew &>/dev/null; then
