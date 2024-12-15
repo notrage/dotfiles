beb7dde26ce695343b841bf209618c803254b212
NotRage <grousson.dylan@gmail.com>
2024-12-15 19:47:49 +0100

    modified scripts/executable_install_apt_packages.sh

--- scripts/executable_install_apt_packages.sh
+++ scripts/executable_install_apt_packages.sh
@@ -536,3 +536,16 @@ zlib1g-dev:amd64 \
 zsh \
 zsh-common 
 
+# Move the fonts files
+SOURCE_DIR="$HOME/.fonts"
+TARGET_DIR="$HOME/.local/share/fonts"
+
+mkdir -p "$TARGET_DIR"
+if [ -d "$SOURCE_DIR" ]; then
+    mv "$SOURCE_DIR"/* "$TARGET_DIR"/
+    echo "Fonts moved successfully to $TARGET_DIR."
+else
+    echo "Source directory $SOURCE_DIR does not exist. Skipping font move."
+fi
+rm -rf "$SOURCE_DIR"
+
